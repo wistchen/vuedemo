7310c0e6b2c78f18e14f7fcdb8e9e045c873fbcb
wistchen <760626525@qq.com>
2019-10-26 17:16:51 +0800

    单文件组件

---
+++ demo1/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <test></test>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import test from './components/test.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld,
+    test
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ demo1/src/components/test.vue
@@ -0,0 +1,28 @@
+<template>
+    <h2>test...</h2>
+</template>
+
+<script>
+    export default {
+        name: "HelloWorld",
+        props: {
+            msg: {
+                type:String,
+                default:"test msg"
+            }
+        },
+        methods:{
+
+        },
+        data(){
+            return{
+
+            }
+        },
+    }
+
+</script>
+
+<style scoped>
+
+</style>
